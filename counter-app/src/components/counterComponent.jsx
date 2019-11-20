f251f27745b719c9b833703ce225d21212e8a74c
nltr <learning@inishant.in>
2019-11-21 01:14:41 +0530

    Adding attributes and JSX examples

--- counter-app/src/components/counterComponent.jsx
+++ counter-app/src/components/counterComponent.jsx
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {};
+  state = {
+    count: 0,
+    src: "https://picsum.photos/10"
+  };
   render() {
     return (
       <React.Fragment>
-        <h1>Hello World</h1>
+        <img src={this.state.src} alt=""></img>
+        <span>{this.formatCounter()}</span>
         <button>Increment</button>
       </React.Fragment>
     );
+  }
+
+  formatCounter() {
+    const { count } = this.state;
+    return count == 0 ? "Zero" : count;
   }
 }
 
