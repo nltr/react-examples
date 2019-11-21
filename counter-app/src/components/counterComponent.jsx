f3bc62f7de87402a081b2003ea6c71e64fa054df
nltr <learning@inishant.in>
2019-11-22 00:10:39 +0530

    working with lists

--- counter-app/src/components/counterComponent.jsx
+++ counter-app/src/components/counterComponent.jsx
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1
+    count: 1,
+    tags: ["1", "2", "3"]
   };
   styles = {
     fontSize: "50px",
@@ -13,19 +14,24 @@ class Counter extends Component {
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCounter()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </React.Fragment>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count == 0 ? "warning" : "primary";
+    classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCounter() {
     const { count } = this.state;
-    return count == 0 ? "Zero" : count;
+    return count === 0 ? "Zero" : count;
   }
 }
 
