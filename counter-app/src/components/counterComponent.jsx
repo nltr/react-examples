e119a7001ebcf4beec9d497edb820183e389133d
nltr <learning@inishant.in>
2019-11-22 00:03:17 +0530

    Adding dyanmic classes to element in a component

--- counter-app/src/components/counterComponent.jsx
+++ counter-app/src/components/counterComponent.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 1
   };
   styles = {
     fontSize: "50px",
@@ -11,12 +11,16 @@ class Counter extends Component {
   render() {
     return (
       <React.Fragment>
-        <span className="badge badge-primary m-2" style={this.styles}>
-          {this.formatCounter()}
-        </span>
+        <span className={this.getBadgeClasses()}>{this.formatCounter()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count == 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCounter() {
